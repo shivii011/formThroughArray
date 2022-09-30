13ef0fe45004c77660eb0330407e95b31735fc2e
shivii011 <shiv011kush011@gamil.com>
2022-09-30 19:55:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+ const object=[
+  {label:"Name",
+    inputType : "text",
+  placeholder:"shivam"
+  },
+  {label:"Email",
+  inputType : "email",
+placeholder:"email@.com"
+},
+{
+  label:"Contect Number",
+  inputType:"number",
+  placeholder:"123"
+},{
+  label:"contect",
+  inputType:"number",
+  placeholder:"123",
+}
+, {label:"Message",
+inputType : "text",
+placeholder:"Give your Suggestions here"
+},
+ ]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {object.map(({label,inputType,placeholder})=>{
+        return(<>
+        <form>
+          {/* <label>{label}</label>
+          <input type={inputType} value={placeholder}/> */}
+
+          <div class="form-group">
+    <label >{label}</label>
+    <input type={inputType} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder={placeholder}/>
+
+  </div>
+        </form>
+        </>
+        )
+      })}
+     <button type="submit" class="btn btn-primary" >Submit</button>
     </div>
   );
 }
